e0706b09a9542faef0fc6bbdd130c861f65207ca
Tom Phowborom <78992395+phowborom@users.noreply.github.com>
2021-07-13 21:33:59 -0700

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,42 @@
 // from data.js
 var tableData = data;
+console.log(tableData);
 
-// YOUR CODE HERE!
+// Reference table body
+var tbody = d3.select("tbody");
+console.log(data);
+
+// Loop Through 'data' and console.log each sighting
+data.forEach(function(ufo){
+    console.log(ufo);
+    var row = tbody.append("tr");
+
+    // Use 'Objects.entries' to console.log each sighting
+    Object.entries(ufo).forEach(function([key,value]){
+        console.log(key,value);
+        var cell = tbody.append("td");
+        cell.text(value);
+    });
+});
+
+// Define event to run filter
+var button = d3.select("#filter-btn");
+button.on("click", function(event){
+    d3.event.preventDefault();
+    tbody.html("");
+
+// User input location field
+var inputElement = d3.select("#datetime"); 
+var inputValue = inputElement.property("value");
+
+// Filter data by user input and print out in each row
+var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+filteredData.forEach(function(dateData){
+    var row=tbody.append("tr");
+
+    Object.entries(dateData).forEach(function([key,value]){
+    var cell=tbody.append("td");
+    cell.text(value);
+        });
+    });
+});
